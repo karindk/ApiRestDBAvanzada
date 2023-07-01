51a5d6cab6a8315ab062ab279200a7640444e824
karindl15 <95378089+karindk@users.noreply.github.com>
2023-07-01 19:04:18 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const router = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
+
+//conexión con mongodb
 mongoose.connect('mongodb://127.0.0.1:27017/Mimbral',{
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -16,8 +18,9 @@ db.once('open',()=>{
     console.log('[*] Conectado a MongoDB\n')
 });
 
+//se declara que se usará formato json
 app.use(express.json())
 
 app.use('/api',router)
-
+//se abre el servidor de la api
 app.listen(port,()=>console.log('[*] Servidor escuchando en el puerto '+port))
